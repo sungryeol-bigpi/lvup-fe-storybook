bb95bbf1ecdc0f09de2535b223620be8d7d1dc2a
rabelais88 <sungryeolp@gmail.com>
2021-09-14 16:18:02 +0900

    :construction: userbadge 관련 자료 수정

--- stories/UserBadge.stories.js
+++ stories/UserBadge.stories.js
@@ -1,5 +1,7 @@
 import UserBadge from "@/views/components/coaching/UserBadge.vue";
 import { setViewport } from "./util/parameters";
+import { users } from './sample'
+const [sampleUser] = users;
 
 export default {
   title: "coaching/UserBadge",
@@ -16,33 +18,13 @@ const Template = (args, { argTypes }) => ({
 });
 
 export const DesktopSmall = Template.bind({});
-DesktopSmall.args = {
-  avatar:
-    "dev/images/user/5c048d7efb1a87695c765cba_1607056860036_1607056860085.jpg",
-  userId: "railguns",
-  nickName: "railguns",
-  upPoint: 30000,
-};
-DesktopSmall.parameters = {
-  ...setViewport("desktopSmall"),
-};
+DesktopSmall.args = { myInfo: sampleUser };
+DesktopSmall.parameters = { ...setViewport("desktopSmall") };
 
 export const TabletPortrait = Template.bind({});
-TabletPortrait.args = {
-  avatar:
-    "dev/images/user/5c048d7efb1a87695c765cba_1607056860036_1607056860085.jpg",
-  userId: "railguns",
-  nickName: "railguns",
-  upPoint: 30000,
-};
+TabletPortrait.args = { myInfo: sampleUser };
 TabletPortrait.parameters = { ...setViewport("tabletPortrait") };
 
 export const MobileMedium = Template.bind({});
-MobileMedium.args = {
-  avatar:
-    "dev/images/user/5c048d7efb1a87695c765cba_1607056860036_1607056860085.jpg",
-  userId: "railguns",
-  nickName: "railguns",
-  upPoint: 30000,
-};
+MobileMedium.args = { myInfo: sampleUser };
 MobileMedium.parameters = { ...setViewport("mobileMedium") };
