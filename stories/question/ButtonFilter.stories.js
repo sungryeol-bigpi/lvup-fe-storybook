96f8b8ec3a96593d92a3d6f47149eb6a89f8e76b
sungryeol-bigpi <40971227+sungryeol-bigpi@users.noreply.github.com>
2021-09-21 18:25:55 +0900

    COG-123 필터링 버튼 추가 (#24)
    * COG-123 필터링 버튼 추가
    
    * COG-123 잘못 들어간 코드 삭제
    
    Co-authored-by: sungryeol park <sungryeolp@gmail.com>

---
+++ stories/question/ButtonFilter.stories.js
@@ -0,0 +1,33 @@
+import ButtonFilter from "@/views/components/coaching/question/ButtonFilter.vue";
+
+export default {
+  title: "coaching/question/ButtonFilter",
+  component: ButtonFilter,
+  parameters: {
+    backgrounds: { default: "coach-grey" },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { ButtonFilter },
+  props: Object.keys(argTypes).filter((k) => k !== "content"),
+  template: `<ButtonFilter v-bind="$props" :class="{active}">${args.content}</ButtonFilter>`,
+});
+
+export const Normal = Template.bind({})
+Normal.args = {
+  content: `전체`,
+  active: false,
+}
+
+export const NormalActive = Template.bind({})
+NormalActive.args = {
+  content: `전체`,
+  active: true,
+}
+
+export const WithExtraActive = Template.bind({})
+WithExtraActive.args = {
+  content: `<span>포지션 : </span><em>탑</em>`,
+  active: true,
+}
