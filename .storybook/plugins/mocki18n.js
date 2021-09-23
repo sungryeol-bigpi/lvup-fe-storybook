2faf06a2fcc4107bf157ee9346e8c5a2ebcd2ce1
rabelais88 <sungryeolp@gmail.com>
2021-09-23 21:18:33 +0900

    :globe_with_meridians: i18n 연결

--- .storybook/plugins/mocki18n.js
+++ .storybook/plugins/mocki18n.js
@@ -1,9 +1,28 @@
+import i18nData from "@/plugins/i18n/data";
+import _get from "lodash/get";
+import BI18n from "@/modules/BI18n";
+import moment from "moment";
+import "@/plugins/i18n/moment.ko";
+
+const defaultLocale = "ko";
+
 export default (Vue, options) => {
-  console.log('i18n mocked')
-  Vue.prototype.$t = (...args) => `$t-${args.join(",")}`;
-  Vue.prototype.$date = Vue.prototype.$t;
-  Vue.prototype.$te = () => true;
-  Vue.prototype.$tc = (...args) => `$tc(${args.join(',')})`
+  console.log("i18n mocked");
+  const $te = (key = "") => _get(i18nData, key) || false;
+  const i18n = new BI18n({ data: { ko: i18nData } });
+  Vue.prototype.$t = (key, opt) => i18n.translate(key, "kr", false, false, opt);
+  Vue.prototype.$date = (time, key = "ymd") => {
+    return moment(+time).format(
+      i18n.translate(`date.${key}`, defaultLocale, false, false)
+    );
+  };
+  Vue.prototype.$fromNow = (time) => {
+    moment.locale(defaultLocale);
+    moment(+time).fromNow();
+  };
+
+  Vue.prototype.$te = $te;
+  Vue.prototype.$tc = (key, opt) => i18n.translate(key, "kr", true, true, opt);
   Vue.prototype.$i18n = (key) => {
     const i18nData = { lang: "ko", country: "kr" };
     return i18nData[key] || "";
---
+++ stories/sample/coachInfo.js
@@ -0,0 +1,147 @@
+export default {
+  "coachId": "5de8a6998057024270ccd1e1",
+  "user": {
+    "userId": "5db2c464cfc74a983c8ae993",
+    "uid": "LV_yabap60ie9",
+    "nickname": "test1_user",
+    "email": "test1_user@test.test",
+    "profileImage": "dev/images/user/5db2c469cfc74a983c8aebba_1616569950138_1616569950593.jpg",
+    "backgroundImageUrl": null,
+    "isEmailVerified": true,
+    "emailVerifiedDatetime": 1614336156093,
+    "selfVerifiedDatetime": null,
+    "point": 5,
+    "recordsCount": 4,
+    "prizesCount": 2,
+    "tournamentsCount": 12,
+    "statsUpdateDatetime": 1578421803485,
+    "state": "NORMAL",
+    "isNormal": true,
+    "isTermsAgree": true,
+    "joinedDatetime": 1543245074991,
+    "mustRetryVerification": false
+  },
+  "tags": [
+    "PARTNER",
+    "PRO"
+  ],
+  "gameId": "5e1d9c64f711ba829bcc4c8c",
+  "reviewRating": 4.5,
+  "reviewCount": 18,
+  "isAdvertising": false,
+  "isLiked": false,
+  "rank": {
+    "code": "DIAMOND_4",
+    "logoUrl": "prod/images/thirdparty/riot/lol/Emblem_Diamond.png"
+  },
+  "status": "ONLINE",
+  "mainHistory": [
+    "프로 코치",
+    "xvsdfsdffsd"
+  ],
+  "recommendLessonPrice": 3000,
+  "enableDoubleBundleSale": true,
+  "enableTripleBundleSale": true,
+  "lessonProducts": [
+    {
+      "lessonProductId": "602e21e971a00c5bc86f524c",
+      "lesson": {
+        "lessonId": "602e21e971a00c5bc86f525c",
+        "name": "리플레이 코칭",
+        "description": [
+          {
+            "thumbnailUrl": "dev/images/lesson/1614753792356.png",
+            "description": "피드백 받을 게임의 리플레이를 준비해 주세요.",
+            "shortDescription": "피드백 받을 게임의 리플레이를 준비해 주세요."
+          },
+          {
+            "thumbnailUrl": "dev/images/lesson/1614753792629.png",
+            "description": "코치님이 해당 리플레이를 분석합니다.",
+            "shortDescription": "코치님이 해당 리플레이를 분석합니다."
+          },
+          {
+            "thumbnailUrl": "dev/images/lesson/1614753792918.png",
+            "description": "분석 내용을 바탕으로 개선 사항을 코칭합니다.",
+            "shortDescription": "분석 내용을 바탕으로 개선 사항을 코칭합니다."
+          }
+        ],
+        "duration": {
+          "unit": "HOURS",
+          "value": 1
+        },
+        "thumbnail": "images/lesson/img_lecture_replay.png"
+      },
+      "price": 0,
+      "coachId": "5de8a6998057024270ccd1e1",
+      "isHide": true
+    },
+    {
+      "lessonProductId": "602e21e971a00c5bc86f524d",
+      "lesson": {
+        "lessonId": "603f2c79f2ad270001cfb49a",
+        "name": "화면 공유로 실시간 코칭",
+        "description": [
+          {
+            "thumbnailUrl": "dev/images/lesson/1614753748019.png",
+            "description": "먼저 화면 공유를 시작 후, 게임을 플레이 합니다.",
+            "shortDescription": "먼저 화면 공유를 시작 후, 게임을 플레이 합니다."
+          },
+          {
+            "thumbnailUrl": "dev/images/lesson/1614753748280.png",
+            "description": "게임 상황에 맞춰 실시간으로 플레이 방향을 코칭해 드립니다.",
+            "shortDescription": "게임 상황에 맞춰 실시간으로 코칭해 드립니다."
+          },
+          {
+            "thumbnailUrl": "dev/images/lesson/1614753733851.png",
+            "description": "게임이 종료된 후, 리플레이를 통해 간단히 한번 더 점검해 드립니다.",
+            "shortDescription": "종료 후, 리플레이를 통해 한번 더 점검해 드립니다."
+          }
+        ],
+        "duration": {
+          "unit": "HOURS",
+          "value": 1
+        },
+        "thumbnail": "images/lesson/img_lecture_sharescreen.png"
+      },
+      "price": 3000,
+      "coachId": "5de8a6998057024270ccd1e1",
+      "isHide": false
+    },
+    {
+      "lessonProductId": "602e21e971a00c5bc86f524e",
+      "lesson": {
+        "lessonId": "603f2cdef2ad270001cfb49b",
+        "name": "1:1 라인전 코칭",
+        "description": [
+          {
+            "thumbnailUrl": "dev/images/lesson/1614753748019.png",
+            "description": "사용자 설정 게임에서 코치님과 만납니다.",
+            "shortDescription": "사용자 설정 게임에서 코치님과 만납니다."
+          },
+          {
+            "thumbnailUrl": "dev/images/lesson/1614753748280.png",
+            "description": "인게임 1:1 라인전 상황에서 코칭해 드립니다.",
+            "shortDescription": "인게임 1:1 라인전 상황에서 코칭해 드립니다."
+          },
+          {
+            "thumbnailUrl": "dev/images/lesson/1614753733851.png",
+            "description": "게임이 종료된 후, 플레이를 간단히 한번 더 점검해 드립니다.",
+            "shortDescription": "종료 후, 플레이를 한번 더 점검해 드립니다."
+          }
+        ],
+        "duration": {
+          "unit": "HOURS",
+          "value": 1
+        },
+        "thumbnail": "images/lesson/img_lecture_lineplay.png"
+      },
+      "price": 3000,
+      "coachId": "5de8a6998057024270ccd1e1",
+      "isHide": false
+    }
+  ],
+  "historyThumbnail": "prod/prod/images/coach/default.png",
+  "private": {
+    "isOfflineMode": false
+  }
+}
