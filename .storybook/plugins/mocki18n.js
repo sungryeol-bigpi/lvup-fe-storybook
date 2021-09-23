e347b2d4d532a0157b2c4b98a266cbca68719bda
rabelais88 <sungryeolp@gmail.com>
2021-09-23 21:38:37 +0900

    :globe_with_meridians: i18n 작동방식 수정

--- .storybook/plugins/mocki18n.js
+++ .storybook/plugins/mocki18n.js
@@ -4,13 +4,13 @@ import BI18n from "@/modules/BI18n";
 import moment from "moment";
 import "@/plugins/i18n/moment.ko";
 
-const defaultLocale = "ko";
+const defaultLocale = "kr";
 
 export default (Vue, options) => {
   console.log("i18n mocked");
   const $te = (key = "") => _get(i18nData, key) || false;
   const i18n = new BI18n({ data: { ko: i18nData } });
-  Vue.prototype.$t = (key, opt) => i18n.translate(key, "kr", false, false, opt);
+  Vue.prototype.$t = (key, opt) => i18n.translate(key, defaultLocale, false, false, opt);
   Vue.prototype.$date = (time, key = "ymd") => {
     return moment(+time).format(
       i18n.translate(`date.${key}`, defaultLocale, false, false)
@@ -18,14 +18,13 @@ export default (Vue, options) => {
   };
   Vue.prototype.$fromNow = (time) => {
     moment.locale(defaultLocale);
-    moment(+time).fromNow();
+    return moment(+time).fromNow();
   };
 
   Vue.prototype.$te = $te;
-  Vue.prototype.$tc = (key, opt) => i18n.translate(key, "kr", true, true, opt);
+  Vue.prototype.$tc = (key, opt) => i18n.translate(key, defaultLocale, true, true, opt);
   Vue.prototype.$i18n = (key) => {
     const i18nData = { lang: "ko", country: "kr" };
     return i18nData[key] || "";
   };
-  Vue.prototype.$fromNow = (value) => `$fromNow(${value})`;
 };
