39dd9c7bbc8c90f2200fa7bc8d437c199cae4230
sungryeol park <sungryeolp@gmail.com>
2021-09-27 17:06:40 +0900

    COG-137 챔프 필터링 모달 추가

--- stories/filter/ModalFilterChampWrap.vue
+++ stories/filter/ModalFilterChampWrap.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <p>선택된 champId:{{ champId }}</p>
     <button @click="openModal">챔프 필터링 모달 열기</button>
   </div>
 </template>
@@ -8,9 +9,16 @@
 import ModalFilterChamp from "@/views/components/coaching/filter/ModalFilterChamp.vue";
 export default {
   components: { ModalFilterChamp },
+  data: () => ({ champId: "Garen" }),
   methods: {
-    openModal() {
-      this.$modal(ModalFilterChamp, {defaultChampId: 'Garen'});
+    async openModal() {
+      try {
+        this.champId = await this.$modal(ModalFilterChamp, {
+          defaultChampId: this.champId,
+        });
+      } catch (err) {
+        console.error('no-champ')
+      }
     },
   },
 };
