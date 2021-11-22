d961e193bf4b97706da55e63c4110b0bd35eecba
rabelais88 <sungryeolp@gmail.com>
2021-11-22 22:48:05 +0900

    :bug: COG-342 i18n 오동작 버그 수정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import VueEasytable from 'vue-easytable'
 import Vue from 'vue';
 // less 관련 오류로 인해 storybook에서 popover 현재는 사용 불가능
-// import popover from '@shared/plugins/popover';
+import popover from '@shared/plugins/popover';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -16,7 +16,8 @@ export const parameters = {
 
 export const decorators = [(story) => {
   Vue.use(VueEasytable);
-  // Vue.use(popover)
+  Vue.use(popover)
+  Vue.prototype.$t = arg => `${arg}`
   return {
     components: { story },
     template: '<story />'
